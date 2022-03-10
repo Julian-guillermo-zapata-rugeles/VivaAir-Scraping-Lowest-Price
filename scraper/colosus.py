e4b92b9a9560d9d0defc120c68fb7ce510714681
Julian Zapata Rugeles <julianruggeles@gmail.com>
2022-03-10 18:57:21 -0500

    - JZ : Versión inicial del proyecto Estructura Básica

---
+++ scraper/colosus.py
@@ -0,0 +1,37 @@
+from cgitb import html
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver    
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+
+
+class Colossus(object):
+    def __init__(self, *args):
+        self.service=Service(ChromeDriverManager().install())
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('headless')
+        self.options.add_argument('window-size=1920x1080')
+        self.options.add_argument("disable-gpu")
+        self._driver = webdriver.Chrome(service=self.service,chrome_options=self.options)
+        self._url = ""
+        self.soup = None
+        
+    def _set_url(self, url):
+        self._url = url
+
+    def _load_url(self):
+        self._driver.get(self._url)
+
+    def _get(self):
+        self._load_url()
+        self.soup = BeautifulSoup(self._driver.page_source,"html.parser")
+
+    def _procesar_respuestas(self , resultado ):
+        return [ i.text for i in resultado]
---
+++ settings/constants.py
@@ -0,0 +1,11 @@
+
+
+class Constants():
+    
+    PATH_DATA_RECENT = "data/last_obtained.csv"
+    PATH_DATA_COMPLETE = "data/all_obtained.csv"
+    DEFAULT_URL = "https://reservas.vivaair.com/#/co/es/booking/resultados?DepartureCity=SMR&ArrivalCity=MDE&DepartureDate=2022-02-01&ReturnDate=2022-02-28&Adults=1&Currency=COP"
+    
+    FROM = "SMR"
+    TOWARDS = "MDE"
+     
